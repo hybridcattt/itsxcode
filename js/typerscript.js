4e27702dca47a9b60df8fd8dec4e7738697e870d
Marina Gornostaeva <gornostaevamv@gmail.com>
2020-02-01 22:35:04 +0100

    Add tracking of twitter button clicks and end of typer animation

--- js/typerscript.js
+++ js/typerscript.js
@@ -1,29 +1,35 @@
-const element = document.querySelector('.xcode');
+function startTyper(onTyperStart, onTyperEnd) {
 
-typer(element, { min: 80, max: 120 })
-.line('')
-.pause(500)
+  const element = document.querySelector('.xcode');
 
-.continue('xCode')
-.pause(500)
-.back(-1, 80)
-.pause(250)
+  onTyperStart()
 
-.continue('-code')
-.pause(500)
-.back('all', 50)
-.pause(500)
+  typer(element, { min: 80, max: 120 })
+  .line('')
+  .pause(500)
 
-.continue('X')
-.pause(120)
-.continue('Code', 70)
-.pause(500)
-.back(-1, 50)
-.pause(100)
+  .continue('xCode')
+  .pause(500)
+  .back(-1, 80)
+  .pause(250)
 
-.continue('code', {min: 150, max: 200})
-.pause(300)
+  .continue('-code')
+  .pause(500)
+  .back('all', 50)
+  .pause(500)
 
-.cursor('false')
-.pause(200)
-.end()
+  .continue('X')
+  .pause(120)
+  .continue('Code', 70)
+  .pause(500)
+  .back(-1, 50)
+  .pause(100)
+
+  .continue('code', {min: 150, max: 200})
+  .pause(300)
+
+  .cursor('false')
+  .pause(200)
+  .run(function() { onTyperEnd() })
+  .end()
+}
