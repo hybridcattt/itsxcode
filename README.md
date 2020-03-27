d88966184f16bd2ddfd36e3d79113b4ab43d8c1f
Marina Gornostaeva <gornostaevamv@gmail.com>
2020-03-27 16:20:57 +0100

    Delete unused code. Update readme

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 Fun little web page explaining how to spell "Xcode" correctly
 
 https://itsxcode.app
+
+## Usage
+
+Customize the terms shown by passing optional parameters: `index.html?c=JavaScript&w=javascript,java%20script`
--- js/typerscript.js
+++ js/typerscript.js
@@ -110,42 +110,6 @@ function startCustomTyper(correct, wrongArray, trackFunction) {
   t.end();
 }
 
-function startTyperXcode(onTyperStart, onTyperEnd) {
-
-  const element = document.querySelector('.xcode');
-
-  onTyperStart()
-
-  typer(element, { min: 80, max: 120 })
-  .line('')
-  .pause(500)
-
-  .continue('xCode')
-  .pause(500)
-  .back(-1, 80)
-  .pause(250)
-
-  .continue('-code')
-  .pause(500)
-  .back('all', 50)
-  .pause(500)
-
-  .continue('X')
-  .pause(120)
-  .continue('Code', 70)
-  .pause(500)
-  .back(-1, 50)
-  .pause(100)
-
-  .continue('code', {min: 150, max: 200})
-  .pause(300)
-
-  .cursor('false')
-  .pause(200)
-  .run(function() { onTyperEnd() })
-  .end()
-}
-
 function commonLength(str1, str2) {
   let i = 0;
   while(i < str1.length && i < str2.length && str1.charAt(i) === str2.charAt(i)) {
